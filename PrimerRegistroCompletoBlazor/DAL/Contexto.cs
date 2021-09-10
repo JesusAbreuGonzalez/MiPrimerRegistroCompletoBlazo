d89d7dab5032add7478d569cd4b4fcb0f3f6c13c
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-09-09 21:06:51 -0400

    Agregando contexto

---
+++ PrimerRegistroCompletoBlazor/DAL/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimerRegistroCompletoBlazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerRegistroCompletoBlazor.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Estudiantes> Estudiantes { get; set; }
+
+    }
+}
