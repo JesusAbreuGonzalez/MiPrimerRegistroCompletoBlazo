f6881b0354dd2091253aacbca8926f3e4c1f4064
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-09-09 20:04:39 -0400

    Creando entidad Estudiantes

---
+++ PrimerRegistroCompletoBlazor/Models/Estudiantes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerRegistroCompletoBlazor.Models
+{
+    public class Estudiantes
+    {
+        [Key]
+        public int EstudianteId { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el nombre")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el teléfono")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir la cédula")]
+        public string Cedula { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir la dirección")]
+        public string Direccion { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
